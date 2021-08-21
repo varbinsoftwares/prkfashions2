e32831936dc4a1a1ca8ae84b8a43ee187fb93a95
idenginfotech <divyasolanki016@gmail.com>
2021-08-21 12:42:05 +0530

    files modified

--- application/views/pages/customer_service.php
+++ application/views/pages/customer_service.php
@@ -29,13 +29,14 @@ $this->load->view('layout/header');
                 <!-- Basics Accordion -->
                 <div id="basicsAccordion" class="mb-12">
                     <div class="row">
-                   <div class="col-sm-6" style="font-size: 25px;">
+                   <div class="col-lg-6 col-sm-12" style="font-size: 25px;">
                     <p>Our customers are our priority. We are 24/7 available for your assistance. We will answer your questions as soon as possible. you can contact us by submitting your query with this <a href="<?php echo site_url('contact');?>">contact form</a> or you can call us on our customer care number : <br><strong>(+91) 95160-41729</strong>.</p>
                     <p> <strong>You can also mail us:</strong> pavitramod11@gmail.com</p>
                     <p><strong>Submit query: </strong>  <a href="<?php echo site_url('contact');?>">Leave us a Message</a></p>
                    </div>
-                   <div  class="col-sm-6">
-                     <img src="<?php echo base_url();?>assets/theme2/images/banner/cservice.jpg" alt="customer services" height="400px" width ="500px">
+                   <div  class="col-lg-6 col-sm-12 ">
+                       
+                     <img src="<?php echo base_url();?>assets/theme2/images/banner/cservice.jpg" alt="customer services" height="400px" width ="500px" style="max-width:350px; ">
                    </div>
                    </div>
                 </div>
