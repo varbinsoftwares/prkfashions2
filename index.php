04cd3c81e79f5d41098545de32319ee456b804c7
octopuscart <dev@develop.com>
2021-08-22 10:21:32 +0530

    cahgnes

--- index.php
+++ index.php
@@ -55,8 +55,8 @@
  */
 //define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
 $baselink = 'http://' . $_SERVER['SERVER_NAME'];
-$checkdebug = strpos($baselink, 'localhost') ? TRUE : FALSE;
-//$checkdebug = TRUE;
+$checkdebug = strpos($baselink, '192.168') ? TRUE : FALSE;
+$checkdebug = TRUE;
 if ($checkdebug) {
     define('ENVIRONMENT', 'development');
 } else{
