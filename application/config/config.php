d9e5e059e1c44e2c4e0928348a466c0bbf301c5e
octopuscart <dev@develop.com>
2021-10-11 08:34:15 +0530

    cahgnes

--- application/config/config.php
+++ application/config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
-date_default_timezone_set('Asia/Hong_Kong');
+date_default_timezone_set('Asia/Kolkata');
 
 /*
   |--------------------------------------------------------------------------
